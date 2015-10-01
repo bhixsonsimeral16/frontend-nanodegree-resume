79d2ffc02afff9b667d979d82cf81177666d17b7
Brian Hixson-Simeral <brian.hixsonsimeral@gmail.com>
2015-10-01 11:02:26 -0500

    started to work on my resume objects. completed two job objects and the bio object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,30 +4,55 @@ This is empty on purpose! Your code to build the resume will go here.
 
  //$("#main").append("Brian Hixson-Simeral");
 
- var name = "Brian Hixson-Simeral";
- var role = "Web Developer";
+// var name = "Brian Hixson-Simeral";
+// var role = "Web Developer";
 
- var formattedName = HTMLheaderName.replace("%data%", name);
- var formattedRole = HTMLheaderRole.replace("%data%", role);
+ //var formattedName = HTMLheaderName.replace("%data%", name);
+ //var formattedRole = HTMLheaderRole.replace("%data%", role);
 
  //$("#header").prepend(formattedRole);
  //$("#header").prepend(formattedName);
 
- var skills = 
- ["tutoring", "leadership", "JS", "Java"]
+ //var skills = 
+ //["tutoring", "leadership", "JS", "Java"]
 
- var bio = {
-
- 	"name" : name,
- 	"age" : 22,
- 	"role" : role,
- 	"skills" : skills
- };
-
+ 
  //$("#main").prepend(bio.name);
 
- var work = {
+var bio = {
 
+ 	"name" : "Brian Hixson-Simeral",
+ 	"age" : 22,
+ 	"role" : "Web Developer",
+ 	"contact" = {
+ 		"phone" = "928-925-4771",
+ 		"email" = "brian.hixsonsimeral@gmail.com",
+ 		"address" = "810 Commons Cir. #965\n
+ 					 Mount Vernon, IA 52314"
+ 	},
+ 	"welcomeMessage" = "Hello!  My name is Brian, and it is my pleasure to meet you today!",
+ 	"skills" = ["JS", "Java", "R", "Leadership", "Tutoring", "Inovative"]
+ 	
+ };
+
+
+ var work = {
+ 	"jobs" = [
+ 	"Peer Consutlant" = {
+ 		"employer" = "Jessica Johanningmeier",
+ 		"title" = "Peer Consutlant",
+ 		"location" = "Cornell College",
+ 		"dates" = "August, 2013 to Present",
+ 		"description" = ""
+ 	},
+ 	"Research" = {
+ 		"employer" = "Kara Beauchamp",
+ 		"title" = "Researcher",
+ 		"location" = "Cornell College",
+ 		"dates" = "June, 2014 to August, 2014",
+ 		"description" = ""
+ 	},
+ 	]
  }
 
  work.city = "Prescott";
@@ -40,5 +65,5 @@ This is empty on purpose! Your code to build the resume will go here.
  education["Institution"] = "Cornell";
  education["Class"] = "CSC315"
 
- $("#main").prepend(work.name);
- $("#main").prepend(education["Class"]);
+ //$("#main").prepend(work.name);
+ //$("#main").prepend(education["Class"]);
