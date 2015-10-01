4d5738f55e2baf4d70ce223c2e26fee1dfb3b888
Brian Hixson-Simeral <brian.hixsonsimeral@gmail.com>
2015-10-01 10:34:06 -0500

    Added object bio and array skills

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-console.log("I love you");
 
- $("#main").append("Brian Hixson-Simeral");
+ //$("#main").append("Brian Hixson-Simeral");
 
  var name = "Brian Hixson-Simeral";
  var role = "Web Developer";
@@ -11,5 +10,18 @@ console.log("I love you");
  var formattedName = HTMLheaderName.replace("%data%", name);
  var formattedRole = HTMLheaderRole.replace("%data%", role);
 
- $("#header").prepend(formattedRole);
- $("#header").prepend(formattedName);
+ //$("#header").prepend(formattedRole);
+ //$("#header").prepend(formattedName);
+
+ var skills = 
+ ["tutoring", "leadership", "JS", "Java"]
+
+ var bio = {
+
+ 	"name" : name,
+ 	"age" : 22,
+ 	"role" : role,
+ 	"skills" : skills
+ };
+
+ $("#main").prepend(bio.name);
