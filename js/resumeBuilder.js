b39aeeab6c4450e025a8775fdc84b1b14682ec66
Brian Hixson-Simeral <brian.hixsonsimeral@gmail.com>
2015-10-01 09:55:27 -0500

    Created a header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,15 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+console.log("I love you");
 
  $("#main").append("Brian Hixson-Simeral");
+
+ var name = "Brian Hixson-Simeral";
+ var role = "Web Developer";
+
+ var formattedName = HTMLheaderName.replace("%data%", name);
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
